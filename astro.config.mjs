e14b080cdafa3e3f564452a3fe787e59de7112c7
jdk1m <jeongdidit@gmail.com>
2025-09-19 10:54:19 -0400

    added rss feed site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 export default defineConfig({
-  // ...
   integrations: [react()],
+   site: "https://first-astro-blog-tutorial.netlify.app/"
 });
